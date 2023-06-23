e624568966778ae92b08e71d8511972f9af4eaea
phin <112971806+phinwww@users.noreply.github.com>
2023-06-23 17:06:14 +0000

    Updated to add lucky mode

--- cogs/qobuz.py
+++ cogs/qobuz.py
@@ -20,7 +20,7 @@ class Qobuz(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def qobuz(self, ctx, arg1):
+    async def qobuz(self, ctx, arg1, *args):
 
         async with ctx.typing():
 
@@ -30,25 +30,54 @@ class Qobuz(commands.Cog):
             qobuz.get_tokens()
             qobuz.initialize_client(email, password, qobuz.app_id, qobuz.secrets)
 
-            qobuz.handle_url(arg1)
+            if arg1 == 'd':
 
-            directory = 'Qobuz Downloads'
-            zipdir = max([os.path.join(directory,d) for d in os.listdir(directory)], key=os.path.getmtime)
-            filename = str(random.randint(0,9999999999999))
-            filetype = '.zip'
-            print(zipdir)
+                dlurl = ', '.join(args)
+                qobuz.handle_url(dlurl)
 
-            shutil.make_archive(filename, 'zip', zipdir)
-            with open(filename + filetype, 'rb') as f:
-                data = f.read()
-            apiurl = "https://pixeldrain.com/api/file/"
-            response = requests.put(apiurl + filename + filetype, data=data)
-            json = response.json()
-            fileid = json["id"]
-            pixeldrain = "https://pixeldrain.com/u/"
+                directory = 'Qobuz Downloads'
+                zipdir = max([os.path.join(directory,d) for d in os.listdir(directory)], key=os.path.getmtime)
+                filename = str(random.randint(0,9999999999999))
+                filetype = '.zip'
+                print(zipdir)
 
-        await ctx.reply(pixeldrain + fileid)
-        os.remove(filename + filetype)
+                shutil.make_archive(filename, 'zip', zipdir)
+                with open(filename + filetype, 'rb') as f:
+                    data = f.read()
+                apiurl = "https://pixeldrain.com/api/file/"
+                response = requests.put(apiurl + filename + filetype, data=data)
+                json = response.json()
+                fileid = json["id"]
+                pixeldrain = "https://pixeldrain.com/u/"
+
+                await ctx.reply(pixeldrain + fileid)
+                os.remove(filename + filetype)
+
+            elif arg1 == 'l':
+
+                dlquery = ' '.join(args)
+                qobuz.lucky_mode(dlquery)
+
+                directory = 'Qobuz Downloads'
+                zipdir = max([os.path.join(directory,d) for d in os.listdir(directory)], key=os.path.getmtime)
+                filename = str(random.randint(0,9999999999999))
+                filetype = '.zip'
+                print(zipdir)
+
+                shutil.make_archive(filename, 'zip', zipdir)
+                with open(filename + filetype, 'rb') as f:
+                    data = f.read()
+                apiurl = "https://pixeldrain.com/api/file/"
+                response = requests.put(apiurl + filename + filetype, data=data)
+                json = response.json()
+                fileid = json["id"]
+                pixeldrain = "https://pixeldrain.com/u/"
+
+                await ctx.reply(pixeldrain + fileid)
+                os.remove(filename + filetype)
+            else:
+                await ctx.reply("invalid argument, did you forget to type .qobuz d for standard mode or .qobuz l for lucky mode?")
+
 
 async def setup(client):
     await client.add_cog(Qobuz(client))
